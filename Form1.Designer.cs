25e0cce5f3416276ff632ed7a11d127fde288b0e
kosta <gudtn2238@gmail.com>
2025-05-09 17:52:24 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace KolorPicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Preview = new System.Windows.Forms.PictureBox();
+            this.txtHex = new System.Windows.Forms.TextBox();
+            this.txtRgb = new System.Windows.Forms.TextBox();
+            this.ColorTimer = new System.Windows.Forms.Timer(this.components);
+            this.BtnPicker = new System.Windows.Forms.Button();
+            this.lblToast = new System.Windows.Forms.Label();
+            this.ToastTimer = new System.Windows.Forms.Timer(this.components);
+            this.BtnAddPalette = new System.Windows.Forms.Button();
+            this.listPalette = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.Preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Preview
+            // 
+            this.Preview.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Preview.Location = new System.Drawing.Point(23, 21);
+            this.Preview.Name = "Preview";
+            this.Preview.Size = new System.Drawing.Size(80, 80);
+            this.Preview.TabIndex = 0;
+            this.Preview.TabStop = false;
+            // 
+            // txtHex
+            // 
+            this.txtHex.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtHex.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtHex.Location = new System.Drawing.Point(123, 29);
+            this.txtHex.Name = "txtHex";
+            this.txtHex.ReadOnly = true;
+            this.txtHex.Size = new System.Drawing.Size(150, 27);
+            this.txtHex.TabIndex = 1;
+            this.txtHex.Click += new System.EventHandler(this.TxtHex_Click);
+            // 
+            // txtRgb
+            // 
+            this.txtRgb.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtRgb.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtRgb.Location = new System.Drawing.Point(123, 67);
+            this.txtRgb.Name = "txtRgb";
+            this.txtRgb.ReadOnly = true;
+            this.txtRgb.Size = new System.Drawing.Size(150, 27);
+            this.txtRgb.TabIndex = 2;
+            this.txtRgb.Click += new System.EventHandler(this.TxtRgb_Click);
+            // 
+            // ColorTimer
+            // 
+            this.ColorTimer.Tick += new System.EventHandler(this.ColorTimer_Tick);
+            // 
+            // BtnPicker
+            // 
+            this.BtnPicker.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnPicker.Location = new System.Drawing.Point(23, 107);
+            this.BtnPicker.Name = "BtnPicker";
+            this.BtnPicker.Size = new System.Drawing.Size(80, 30);
+            this.BtnPicker.TabIndex = 3;
+            this.BtnPicker.Text = "PICK!";
+            this.BtnPicker.UseVisualStyleBackColor = true;
+            this.BtnPicker.Click += new System.EventHandler(this.BtnPicker_Click);
+            // 
+            // lblToast
+            // 
+            this.lblToast.AutoSize = true;
+            this.lblToast.BackColor = System.Drawing.Color.LightYellow;
+            this.lblToast.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblToast.Location = new System.Drawing.Point(219, 380);
+            this.lblToast.Name = "lblToast";
+            this.lblToast.Size = new System.Drawing.Size(41, 17);
+            this.lblToast.TabIndex = 4;
+            this.lblToast.Text = "label1";
+            this.lblToast.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblToast.Visible = false;
+            // 
+            // ToastTimer
+            // 
+            this.ToastTimer.Interval = 2000;
+            this.ToastTimer.Tick += new System.EventHandler(this.ToastTimer_Tick);
+            // 
+            // BtnAddPalette
+            // 
+            this.BtnAddPalette.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnAddPalette.Location = new System.Drawing.Point(296, 128);
+            this.BtnAddPalette.Name = "BtnAddPalette";
+            this.BtnAddPalette.Size = new System.Drawing.Size(179, 41);
+            this.BtnAddPalette.TabIndex = 5;
+            this.BtnAddPalette.Text = "+ 팔레트에 추가";
+            this.BtnAddPalette.UseVisualStyleBackColor = true;
+            // 
+            // listPalette
+            // 
+            this.listPalette.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listPalette.HideSelection = false;
+            this.listPalette.Location = new System.Drawing.Point(123, 175);
+            this.listPalette.Name = "listPalette";
+            this.listPalette.Size = new System.Drawing.Size(352, 237);
+            this.listPalette.TabIndex = 6;
+            this.listPalette.UseCompatibleStateImageBehavior = false;
+            this.listPalette.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "색상";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "HEX";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "RGB";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "라벨";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(494, 434);
+            this.Controls.Add(this.BtnAddPalette);
+            this.Controls.Add(this.lblToast);
+            this.Controls.Add(this.BtnPicker);
+            this.Controls.Add(this.txtRgb);
+            this.Controls.Add(this.txtHex);
+            this.Controls.Add(this.Preview);
+            this.Controls.Add(this.listPalette);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kolor Picker";
+            ((System.ComponentModel.ISupportInitialize)(this.Preview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Preview;
+        private System.Windows.Forms.TextBox txtHex;
+        private System.Windows.Forms.TextBox txtRgb;
+        private System.Windows.Forms.Timer ColorTimer;
+        private System.Windows.Forms.Button BtnPicker;
+        private System.Windows.Forms.Label lblToast;
+        private System.Windows.Forms.Timer ToastTimer;
+        private System.Windows.Forms.Button BtnAddPalette;
+        private System.Windows.Forms.ListView listPalette;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KolorPicker
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        public static extern uint GetPixel(IntPtr hdc, int x, int y);
+
+        [DllImport("user32.dll")]
+        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hdc);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPicker_Click(object sender, EventArgs e)
+        {
+            if(ColorTimer.Enabled)
+            {
+                ColorTimer.Stop();
+            }
+            else 
+            {
+                ColorTimer.Start();
+            }
+        }
+
+        private Color GetCursorColor()
+        {
+            Point cursorPos = Cursor.Position;
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            uint pixel = GetPixel(hdc, cursorPos.X, cursorPos.Y);
+            ReleaseDC(IntPtr.Zero, hdc);
+
+            Color color = Color.FromArgb(
+                (int)(pixel & 0x000000FF),           // R
+                (int)(pixel & 0x0000FF00) >> 8,      // G
+                (int)(pixel & 0x00FF0000) >> 16      // B
+            );
+
+            return color;
+        }
+
+        private void ColorTimer_Tick(object sender, EventArgs e)
+        {
+            Color currentColor = GetCursorColor();
+            Controls["txtHex"].Text = $"#{currentColor.R:X2}{currentColor.G:X2}{currentColor.B:X2}";
+            Controls["txtRgb"].Text = $"{currentColor.R}, {currentColor.G}, {currentColor.B}";
+            Controls["Preview"].BackColor = currentColor;
+        }
+
+        private void TxtHex_Click(object sender, EventArgs e)
+        {
+            if(sender is TextBox textBox)
+            {
+                string text = textBox.Text.Trim();
+                Clipboard.SetText(text);
+                ShowToast("색상 HEX가 복사완료!");
+            }
+        }
+
+        private void TxtRgb_Click(object sender, EventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                string text = textBox.Text.Trim();
+                Clipboard.SetText(text);
+                ShowToast("색상 RGB가 복사완료!");
+            }
+        }
+
+        private void ShowToast(string message)
+        {
+            lblToast.Text = message;
+            int x = (ClientSize.Width - lblToast.PreferredWidth) / 2;
+            lblToast.Location = new Point(x, lblToast.Location.Y);
+
+            lblToast.Visible = true;
+            ToastTimer.Start();
+        }
+
+        private void ToastTimer_Tick(object sender, EventArgs e)
+        {
+            lblToast.Visible = false;
+            ToastTimer.Stop();
+        }
+    }
+}
